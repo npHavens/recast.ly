94c98654d0686202f21d2ed368415b34d1682f4f
Nick Havens <npahavens@gmail.com>
2017-07-08 14:50:59 -0500

    added render call to index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,3 +13,4 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
